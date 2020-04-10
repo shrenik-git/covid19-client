bae30bd4e03bd9af2589ef21a8bb7974e0b3bcd7
Shrenik Shah <shrenik@gmail.com>
2020-04-10 12:29:20 -0400

    Google Analystics Integration!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare let ga: Function;
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'covid19-client';
+  title = 'Covid19 - Dashboard';
 
-}
+  constructor(public router: Router) {
+
+    // subscribe to router events and send page views to Google Analytics
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+        console.log(event.urlAfterRedirects);
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+
+      } // if
+
+    }); // subscribe
+
+  } // constructor
+
+} // AppComponent
